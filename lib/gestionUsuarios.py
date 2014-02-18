e1a5398bbfa0da60e42fda46bfcca4a6206a9ac0
Saul <sblanco@tsc.uc3m.es>
2014-02-18 14:30:40 +0100

    Creado el ejemplo 8 para ver todos los datos del usuario, no sólo las fechas. Se usará, además, para la versión gráfica

--- lib/gestionUsuarios.py
+++ lib/gestionUsuarios.py
@@ -17,12 +17,17 @@ class gestionUsuarios:
 	__usuarios = {}
 	__db	   = ''
 	__ldap_con = ''
+	__basedn   = '' 
 
 
 	#When searching the internet, you will find many formulas that use the number 11644473600. This 
 	#is the number of seconds since 31-12-1601, which is used to calculate the "accountExpires"
 	#value. The values "lastLogon" and "lastLogonTimeStamp" however use 01-01-1601 as the date to
 	#calculate this value.
+	
+	'''
+		FUNCIONES BASICAS PARA CONVERTIR UN TIMESTAMP DE ACTIVEDIRECTORY EN FECHAS:
+	'''	
 	def __convWinTime ( self, WinTimestamp, accExp = 0 ):
 
 		MagicNumber = 11644473600
@@ -41,11 +46,30 @@ class gestionUsuarios:
 
 		return ( (UnixTimesTamp *10000000) + MagicNumber )
 
+	'''
+		*********************************************************
+	'''
+
+	'''
+		Algunas fechas de caducidad no se encuentran en formato timestamp, sino en formato cadena. De forma que
+		2014/02/20 se encuentra representado como 20140220. Este procedimiento lo pone en el formato mas legible (con las /)
+	'''
+	def __convStringDate (self, stringDate):
+		return stringDate [:4] + '/' + stringDate[4:6] + '/' + stringDate[6:8] 
 
 
 	def getUsr ( self, login ):
 		return self.__usuarios[ login ]
+	
+	'''
+		*********************************************************
+	'''
 
+	
+	'''
+		SACAMOS LAS CUENTAS CADUCADAS DESDE, AL MENOS, LOS DIAS PASADOS COMO PARAMETROS.
+		Debemos ignorar las fechas de caducidad que esten en blanco:
+	'''
 	def getCaducadas ( self, dias ):
 
 		lista = {}
@@ -56,7 +80,13 @@ class gestionUsuarios:
 				lista [k] = v
 
 		return lista
-
+	'''
+		*********************************************************
+	'''
+	
+	'''
+		USUARIOS QUE NO HAN ENTRADO AL SISTEMA DESDE LOS DIAS PASADOS COMO PARAMETRO:
+	'''
 	def getSinLogin (self, dias ):
 		lista = {}
 		for k,v in self.__usuarios.iteritems():
@@ -64,8 +94,14 @@ class gestionUsuarios:
 				lista [k] = v
 
 		return lista
+	
+	'''
+		*********************************************************
+	'''	
 
-
+	'''
+		FIJAR FECHA DE CADUCIDAD DE UN USUARIO:
+	'''
 	def setFechaCaducidad ( self, cn, fecha ):
 		tstamp = int (fecha.strftime("%s"))
 		#print tstamp
@@ -77,6 +113,15 @@ class gestionUsuarios:
 		attrib = [(ldap.MOD_REPLACE, 'accountExpires', fecha )]
 		self.__ldap_con.modify_s( cn, attrib)   
 
+	
+	'''
+		*********************************************************
+	'''
+
+
+	'''
+		BORRAR EL USUARIO PASADO COMO PARAMETRO:
+	'''		
 	def borrarUsuario (self, login):
 		salida = [0,'Descripcion error']
 
@@ -88,17 +133,96 @@ class gestionUsuarios:
 			salida [1] = e
 		return salida
 
+	'''
+		*********************************************************
+	'''
 
+	'''
+		DE MOMENTO SIN USO
+	'''
 	def generarInforme ( self ):
 
 		self.__db = db.DataBase ( 'prueba' )
+		
+	
+	'''
+		*********************************************************
+	'''	
+	
+	'''
+		ADEMAS DE LA INFORMACION BASICA (fechas, login etc) EL USUARIO TIENE ASOCIADOS GRAN CANTIDAD DE DATOS
+		Este procedimiento se encarga de sacar todos los datos asociados a un usuario y entregarlos como un diccionario en formato
+		['nombreAtributo'] = valor
+	'''		
+	def getUserAllData (self, login ):
 
+		scope = ldap.SCOPE_SUBTREE
+		#filter = "(&(objectClass=user)(name=sblanco))"
+		filter = "(&(objectClass=user)(name=" + login + "))"
+		#attributes = ['dn','sAMAccountName','accountExpires','lastLogon']
+		attributes = ['*']
+		result = self.__ldap_con.search_s(self.__basedn, scope, filter, attributes)
+		
+		datos = dict (result)
+		#for k, v in datos.iteritems():
+		#	print '------ %s \n' % k
+		
+		cad = self.__basedn.replace ('cn','CN').replace ('dc','DC').replace (' ','')
+		#usuario = datos ['CN=sblanco,CN=Users,DC=tsc,DC=uc3m,DC=es'] 
+		usuario = datos ['CN=' + login + ',' + cad]
+		
+		salida = {}
+		
+		for d in usuario:
+
+			salida [d] = usuario[ d ] [0]
+		
+		'''
+			Las fechas en formato horrible (timestamp o string) son convertidas a cosas legibles por el usuario:
+		'''
+					
+		salida ['badPasswordTime'] 			= self.__convWinTime (int ( salida ['badPasswordTime'] )  )
+		salida ['pwdLastSet'] 				= self.__convWinTime (int ( salida ['pwdLastSet'] )  )
+		salida ['lastLogonTimestamp'] 		= self.__convWinTime (int ( salida ['lastLogonTimestamp'] )  )
+		salida ['accountExpires'] 			= self.__convWinTime (int ( salida ['accountExpires'] )  )		
+		salida ['lastLogon'] 				= self.__convWinTime (int ( salida ['lastLogon'] )  )
+		salida ['whenChanged']	 			= self.__convStringDate ( salida ['whenChanged'] )
+		salida ['whenCreated']	 			= self.__convStringDate ( salida ['whenCreated'] )
+		
+		
+		return salida
+
+	'''
+		*********************************************************
+	'''			
+			
+	'''
+		Devolvemos todos los usuarios del sistema con la informacion basica (login, cn, fechas...) Si queremos informacion mas concreta
+		de un usuario debemos usar el procedimiento anterior self.getUserAllData
+	'''
+	def getAllUsers ( self ):
+		return self.__usuarios
+	
+	
+	'''
+		El constructor de la clase recibe los datos de conexion y crea un diccionario con lista que se guarda en la clase como privado
+		El diccionaro es del tipo:
+		[usuario][atributo] = valor
+		
+	'''
+	
+	'''
+		*********************************************************
+	'''		
 
 	def __init__ (self,nombre, passwd, basedn, servidor):
 
 		self.__ldap_con = ldap.initialize( 'ldap://' + servidor )
 
 		try:
+			
+			self.__basedn = basedn
+			
 			self.__ldap_con.protocol_version = ldap.VERSION3
 			self.__ldap_con.set_option(ldap.OPT_REFERRALS, 0)
 			self.__ldap_con.simple_bind_s( nombre, passwd)
@@ -167,12 +291,17 @@ class gestionUsuarios:
 		except Exception as e:
 			print(e)
 
+	'''
+		*********************************************************
+	'''
+
 
 	def __del__ ( self ):
 			self.__ldap_con.unbind ()
 
-	def getAllUsers ( self ):
-		return self.__usuarios 
+	'''
+		*********************************************************
+	'''
 
 
 class bcolors:
